070c83ab9251c7d7755388b94865becf48af99ee
422UR4H <tiagosindra@id.uff.br>
2024-01-18 15:11:11 -0300

    :tada: Initial commit

---
+++ src/main/java/com/holidayzer/api/controllers/HealthController.java
@@ -0,0 +1,14 @@
+package com.holidayzer.api.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+  @GetMapping
+  public String getHealth() {
+    return "I'm OK!";
+  }
+}
